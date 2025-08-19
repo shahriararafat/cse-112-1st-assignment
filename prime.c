6624734ba5b4d4401b3f8821364acb5be45a8866
Shahriar Arafat <arafatshahriar98@gmail.com>
2025-08-19 12:50:47 +0600

    some change

--- prime.c
+++ prime.c
@@ -1,46 +1,42 @@
-
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
-
     int n;
     scanf("%d", &n);
 
-
     if (n == 2)
     {
-        printf("The given number is  prime");
-        return;
+        printf("The given number is prime");
+        return 0;
     }
+
     if (n % 2 == 0 || n == 1)
     {
         printf("The given number is not prime");
-        return;
+        return 0;
     }
 
+    int flag = 1;  
 
-    int flag = 1;
     for (int i = 2; i <= sqrt(n); i++)
     {
-
-        if(n % i == 0)
+        if (n % i == 0)  
         {
-
-            flag = 0;
+            flag = 0;    
             break;
         }
     }
-
-    if (flag = 1)
+    if (flag == 1)
     {
-
         printf("The given number is prime");
     }
     else
     {
-
-                printf("The given number is not prime");
-
+        printf("The given number is not prime");
     }
+
+    return 0;
 }
+ 
